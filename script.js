299bcddb5f519cc7234c7fdd6c50cc1f10a8e348
xatudrip <kdmxatu@gmail.com>
2026-04-13 08:58:36 +0000

    commit 4/13

---
+++ script.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal(".bajaimg", 
+    { duration: 1000 },
+    { opacity: 0.5 },
+    { origin: 'left'}
+)
+
+ScrollReveal().reveal(".clipsimg", 
+    { duration: 1000 },
+    { opacity: 0.5 },
+    { origin: 'right'}
+)
+
+ScrollReveal().reveal(".bothcats", 
+    { duration: 1000 },
+    { opacity: 0.5 },
+    { origin: 'bottom'}
+)
+
+ScrollReveal().reveal(".guestcatimg", 
+    { duration: 1000 },
+    { opacity: 0.5 },
+    { origin: 'top'}
+)
